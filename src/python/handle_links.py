e3896f7f0af6fe64ccbe3cc8c27c908e60c8f8e0
Gustav Rasmussen <gcr84@hotmail.com>
2025-01-19 21:07:36 +0100

    chore: fix unit test

--- src/python/handle_links.py
+++ src/python/handle_links.py
@@ -11,7 +11,7 @@ def add_paragraph(func):
     def wrapper(*args, **kwargs):
         html = '<p align="center">\n\t'
         html += func(*args, **kwargs)
-        html = '\n</p>'
+        html += '\n</p>'
         return html
     return wrapper
 
@@ -21,4 +21,3 @@ def convert_url(google_drive_item_url: str) -> str:
     tmp = google_drive_item_url.split("/d/")[-1]
     _id = tmp.removesuffix("/view?usp=sharing")
     return f'<img src="https://lh3.googleusercontent.com/d/{_id}" width="400"/>'
-
--- src/python/test/test_handle_links.py
+++ src/python/test/test_handle_links.py
@@ -1,10 +1,8 @@
 
-from src.python.handle_links import convert_url  # type: ignore
+from handle_links import convert_url  # type: ignore
 
 
 def test_convert_url():
     url = "https://drive.google.com/file/d/1Jdyu94wuNAbiKldtgwWPJYqGSeLeGV5i/view?usp=sharing"
-    expected = '''<p align="center">
-        <img src="https://lh3.googleusercontent.com/d/17ggezi9SP4a1_8GIzMJxV8b1VBbzhMFZ" width="400"/>
-    </p>'''
+    expected = '<p align="center">\n\t<img src="https://lh3.googleusercontent.com/d/1Jdyu94wuNAbiKldtgwWPJYqGSeLeGV5i" width="400"/>\n</p>'
     assert convert_url(url) == expected
